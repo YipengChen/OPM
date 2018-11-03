e3e014b374f61a5938ee9572c82e5b8f2f9aec6a
YipengChen <chenypmail@163.com>
2018-11-03 14:22:42 +0800

    TestVGG16_market1501_v0.1.1
    20181103_layer1_NoMetricLoss_market1501_VGG16Test_24_8_512_NoBN

--- cyp_train.sh
+++ cyp_train.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=1 python script/experiment/train_pcb.py \
 --only_test false \
 --dataset market1501 \
 --trainset_part trainval \
---exp_dir Exp/20181103_layer1_NoMetricLoss_market1501_VGG16Test(24,8,512,NoBN) \
+--exp_dir Exp/20181103_layer1_NoMetricLoss_market1501_VGG16Test_24_8_512_NoBN \
 --steps_per_log 20 \
 --epochs_per_val 1 \
 --num_layers 1 \
