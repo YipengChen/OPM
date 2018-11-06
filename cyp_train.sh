78c77c99fb114dcaee6e403188b02a8d056030e5
YipengChen <chenypmail@163.com>
2018-11-06 21:11:36 +0800

    TrainOnSomeDatasets

--- cyp_train.sh
+++ cyp_train.sh
@@ -5,8 +5,41 @@ CUDA_VISIBLE_DEVICES=1 python script/experiment/train_pcb.py \
 --only_test false \
 --dataset market1501 \
 --trainset_part trainval \
---exp_dir Exp/20181103_layer1_NoMetricLoss_market1501_VGG16Test_24_8_512_NoBN \
+--exp_dir Exp/20181103_layer2_NoMetricLoss_market1501_VGG16Test_24_8_512_NoBN \
 --steps_per_log 20 \
 --epochs_per_val 1 \
---num_layers 1 \
+--num_layers 2 \
 --weight_metric_loss 0
+
+CUDA_VISIBLE_DEVICES=1 python script/experiment/train_pcb.py \
+-d '(1,)' \
+--only_test false \
+--dataset market1501 \
+--trainset_part trainval \
+--exp_dir Exp/20181103_layer3_NoMetricLoss_market1501_VGG16Test_24_8_512_NoBN \
+--steps_per_log 20 \
+--epochs_per_val 1 \
+--num_layers 3 \
+--weight_metric_loss 0
+
+CUDA_VISIBLE_DEVICES=1 python script/experiment/train_pcb.py \
+-d '(1,)' \
+--only_test false \
+--dataset market1501 \
+--trainset_part trainval \
+--exp_dir Exp/20181103_layer3_2LayerMetricLoss_w0.5_market1501_VGG16Test_24_8_512_NoBN \
+--steps_per_log 20 \
+--epochs_per_val 1 \
+--num_layers 3 \
+--weight_metric_loss 0.5
+
+CUDA_VISIBLE_DEVICES=1 python script/experiment/train_pcb.py \
+-d '(1,)' \
+--only_test false \
+--dataset market1501 \
+--trainset_part trainval \
+--exp_dir Exp/20181103_layer3_2LayerMetricLoss_w1_market1501_VGG16Test_24_8_512_NoBN \
+--steps_per_log 20 \
+--epochs_per_val 1 \
+--num_layers 3 \
+--weight_metric_loss 1
--- cyp_train2.sh
+++ cyp_train2.sh
@@ -5,8 +5,41 @@ CUDA_VISIBLE_DEVICES=0 python script/experiment/train_pcb.py \
 --only_test false \
 --dataset cuhk03 \
 --trainset_part trainval \
---exp_dir Exp/20181103_layer1_NoMetricLoss_cuhk03_VGG16Test_24_8_512_NoBN \
+--exp_dir Exp/20181103_layer2_NoMetricLoss_cuhk03_VGG16Test_24_8_512_NoBN \
 --steps_per_log 20 \
 --epochs_per_val 1 \
---num_layers 1 \
+--num_layers 2 \
 --weight_metric_loss 0
+
+CUDA_VISIBLE_DEVICES=0 python script/experiment/train_pcb.py \
+-d '(0,)' \
+--only_test false \
+--dataset cuhk03 \
+--trainset_part trainval \
+--exp_dir Exp/20181103_layer3_NoMetricLoss_cuhk03_VGG16Test_24_8_512_NoBN \
+--steps_per_log 20 \
+--epochs_per_val 1 \
+--num_layers 3 \
+--weight_metric_loss 0
+
+CUDA_VISIBLE_DEVICES=0 python script/experiment/train_pcb.py \
+-d '(0,)' \
+--only_test false \
+--dataset cuhk03 \
+--trainset_part trainval \
+--exp_dir Exp/20181103_layer3_2LayerMetricLoss_w0.5_cuhk03_VGG16Test_24_8_512_NoBN \
+--steps_per_log 20 \
+--epochs_per_val 1 \
+--num_layers 3 \
+--weight_metric_loss 0.5
+
+CUDA_VISIBLE_DEVICES=0 python script/experiment/train_pcb.py \
+-d '(0,)' \
+--only_test false \
+--dataset cuhk03 \
+--trainset_part trainval \
+--exp_dir Exp/20181103_layer3_2LayerMetricLoss_w1_cuhk03_VGG16Test_24_8_512_NoBN \
+--steps_per_log 20 \
+--epochs_per_val 1 \
+--num_layers 3 \
+--weight_metric_loss 1
