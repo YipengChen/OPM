7c1d452d997c2cfb274f2ad572a415ff48d4a22c
YipengChen <chenypmail@163.com>
2018-11-06 21:16:39 +0800

    v0.1.1

--- cyp_train.sh
+++ cyp_train.sh
@@ -3,21 +3,10 @@
 CUDA_VISIBLE_DEVICES=1 python script/experiment/train_pcb.py \
 -d '(1,)' \
 --only_test false \
---dataset duke \
+--dataset market1501 \
 --trainset_part trainval \
---exp_dir Exp/20181006_layer3_2LayerMetricLoss_w2.0_duke \
+--exp_dir Exp/20181106_layer3_2LayerMetricLoss_w1.0_market1501 \
 --steps_per_log 20 \
 --epochs_per_val 1 \
 --num_layers 3 \
---weight_metric_loss 2.0
-
-CUDA_VISIBLE_DEVICES=1 python script/experiment/train_pcb.py \
--d '(1,)' \
---only_test false \
---dataset duke \
---trainset_part trainval \
---exp_dir Exp/20181006_layer3_2LayerMetricLoss_w5.0_duke \
---steps_per_log 20 \
---epochs_per_val 1 \
---num_layers 3 \
---weight_metric_loss 5.0
+--weight_metric_loss 1.0
