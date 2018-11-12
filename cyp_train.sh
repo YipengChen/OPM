5390a68f0380e3158296f4575d9179cac767bcd6
YipengChen <chenypmail@163.com>
2018-11-12 19:19:11 +0800

    v0.1.12

--- cyp_train.sh
+++ cyp_train.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-CUDA_VISIBLE_DEVICES=1 python script/experiment/train_pcb.py \
--d '(1,)' \
+CUDA_VISIBLE_DEVICES=0 python script/experiment/train_pcb.py \
+-d '(0,)' \
 --only_test false \
 --dataset market1501 \
 --trainset_part trainval \
---exp_dir Exp/20181106_layer3_2LayerMetricLoss_w1.0_market1501 \
+--exp_dir Exp/20181112_layer3_2LayerMetricLoss_w1.0_market1501 \
 --steps_per_log 20 \
 --epochs_per_val 1 \
 --num_layers 3 \
---weight_metric_loss 1.0
+--weight_metric_loss 1.0	
